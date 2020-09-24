3ff21d006794d8bbef29204d4a408752d014c639
Mustafa Arslan <48215320+mustafaarslan1@users.noreply.github.com>
2020-09-24 14:53:16 +0300

    Passport Login Sistemi Entegresi
    Sisteme Passport Login Sistemini entegre ettim. Postman aracılığı ile testlerini gerçekleştirdim.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# clevel-blog
- 
+<h1>C-level Blog Sistemi</h1>
+<h5>1. Postman</h5>
+<p>
+Projeyi postman'de açmak için: <a href="https://www.getpostman.com/collections/c579ae86910bbaffc4a8">https://www.getpostman.com/collections/c579ae86910bbaffc4a8</a><br>
+İçeri import edildikten sonra; projeye ait environment içine {{api}} isimli değişken tanımlanarak proje adresi (valet için; http://blog.test/api) eklenir.<br>
+Değişken eklendikten sonra, proje pre-request scriptler sayesinde tüm işlemlerini otomatik oluşturulan tokenlar vasıtasıyla gerçekleştirir.
+</p>
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'App\Http\Controllers\API\LoginController@login');
+    Route::post('signup', 'App\Http\Controllers\API\LoginController@signup');
+
+    Route::group([
+        'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', 'App\Http\Controllers\API\LoginController@logout');
+        Route::get('user', 'App\Http\Controllers\API\LoginController@user');
+    });
+});
