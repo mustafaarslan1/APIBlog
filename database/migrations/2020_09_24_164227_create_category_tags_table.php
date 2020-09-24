d323c7fc1f686d0cbb9a85e6bdc4e36a54d85eb8
Mustafa Arslan <48215320+mustafaarslan1@users.noreply.github.com>
2020-09-24 19:43:33 +0300

    category_tags tablosu ve CategoryTag modeli oluşturdum

---
+++ database/migrations/2020_09_24_164227_create_category_tags_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_tags', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id')->unsigned();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->unsignedBigInteger('tag_id')->unsigned();
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_tags');
+    }
+}
